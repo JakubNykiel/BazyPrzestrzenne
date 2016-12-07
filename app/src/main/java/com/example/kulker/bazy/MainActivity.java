391578b997a3e2ef6569677b63d0024573793bc6
DESKTOP-PAWEL\Paweł <pawel.mstowski@gmail.com>
2016-12-07 20:46:01 +0100

    Wczytywanie planu AGH

--- app/src/main/java/com/example/kulker/bazy/MainActivity.java
+++ app/src/main/java/com/example/kulker/bazy/MainActivity.java
@@ -1,21 +1,59 @@
 package com.example.kulker.bazy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.LayerRasterizer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
+import com.esri.android.map.FeatureLayer;
+import com.esri.android.map.MapView;
+import com.esri.core.geodatabase.ShapefileFeatureTable;
+import com.esri.core.renderer.SimpleRenderer;
+import com.esri.core.symbol.SimpleFillSymbol;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    RelativeLayout main;
+    MapView mapView;
+    //com.google.android.gms.maps.MapView mapView;
+    ShapefileFeatureTable shapefileFeatureTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        database = FirebaseDatabase.getInstance();
+        main = (RelativeLayout)findViewById(R.id.activity_main);
+        mapView = (MapView)main.findViewById(R.id.map);
+        String shapeFilePath = "/storage/emulated/0/budynki.shp";
 
-        myRef.setValue("Hello, World!");
+        File shapefile = new File(shapeFilePath);
+
+
+        try {
+            shapefileFeatureTable = new ShapefileFeatureTable(shapefile.getAbsolutePath());
+        } catch (FileNotFoundException ex) {
+            Toast.makeText(this, "ShapeFile Not Found!!!", Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+            return;
+        }
+
+
+        FeatureLayer featureLayer = new FeatureLayer(shapefileFeatureTable);
+        featureLayer.setRenderer(new SimpleRenderer(new SimpleFillSymbol(
+               getResources().getColor(android.R.color.holo_blue_bright),
+                SimpleFillSymbol.STYLE.SOLID)));
+        mapView.addLayer(featureLayer);
+
+
     }
 
 
